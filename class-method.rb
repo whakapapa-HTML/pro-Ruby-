b2bad79f3dd4d07d59d3ff72713f245df41cbded
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-06-17 09:43:20 +0900

    モジュール

--- class-method.rb
+++ class-method.rb
@@ -1,6 +1,6 @@
 class User
-    
-  attr_reader :name 
+
+  attr_reader :name
 
   def initialize(name,weight)
     @name = name
---
+++ monkey_patch.rb
@@ -0,0 +1,22 @@
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  def hello 
+    "Hello, #{@name}!"
+  end
+end
+
+
+
+class User
+  alias hello_original hello
+
+  def hello
+    "#{hello_original}じゃなくて、#{@name}さん、こんにちは"
+  end
+end
+
+user = User.new('Alice')
+puts user.hello
