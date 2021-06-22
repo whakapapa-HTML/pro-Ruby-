43abf368c2eef349f69b98afc543bb074da5d999
uriah.heep0810@gmail.com <uriah.heep0810@gmail.com>
2021-06-22 10:33:13 +0900

    Proc test

---
+++ effects.rb
@@ -0,0 +1,8 @@
+module Effects
+  def self.reverse
+    ->(words) do
+      words.split('').map(&:reverse).join('')
+    end
+  end
+end
+
---
+++ multiple_proc.rb
@@ -0,0 +1,11 @@
+def greeting(proc_1,proc_2,proc_3)
+  puts proc_1.call('おはよう')
+  puts proc_2.call('こんにちは')
+  puts proc_3.call('こんばんは')
+end
+
+shuffle_proc = lambda{ |text| text.chars.shuffle.join }
+repeat_proc = lambda{ |text| text * 2}
+question_proc = lambda{|text| "#{text}?"}
+
+greeting(shuffle_proc, repeat_proc,  question_proc)
