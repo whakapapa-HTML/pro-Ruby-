46d41a3118b1b1cd3608764f0d84b7ced6dcb941
uriah.heep0810@gmail.com <uriah.heep0810@gmail.com>
2021-07-17 22:47:41 +0900

    class_product

--- different.rb
+++ different.rb
@@ -1,19 +1,4 @@
-module Baseball
-  class Second
-    def initialize(player,uniform_number)
-      @player =player
-      @unifirm_number =  uniform_number
-    end
-  end
-end
 
-module Clock
-  class Second
-    def  initialize(digits)
-      @digits =digits
-    end
-  end
-end
 
 
 user =  Baseball::Second.new('Alice',13)
